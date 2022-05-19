23594a73227332bb441dcc44a0077ab05f4ef3d8
mihirarya1 <73761156+mihirarya1@users.noreply.github.com>
2022-05-19 14:48:43 -0700

    Update part2Server.c

--- TelnetProtocol/part2Server.c
+++ TelnetProtocol/part2Server.c
@@ -1,7 +1,5 @@
 /*
 NAME: Mihir Arya
-EMAIL: mihirarya@ucla.edu
-ID: 705126618
 */
 
 /* 
@@ -37,12 +35,13 @@ int setCompress=0;
 
 
 void exitOut(int exitCode)
-{
+{ 
+  // code to safetly exit out 
   if (childKilled==0) // if child was already killed then dont kill                                                                              
     if ( kill(ChildID,SIGINT) < 0 )
       { fprintf(stderr, "Failure when killing child with message %s\n", strerror(errno)); exit(1); }
 
-  if(setCompress)
+  if(setCompress) // close compression paradigms if they were opened
     {
       if ( deflateEnd(&out_stream) == Z_STREAM_ERROR ) // not technically a sys call
         fprintf(stderr, "deflateEnd() failure at server \n");
@@ -54,6 +53,7 @@ void exitOut(int exitCode)
 
 int myread(int fd, void *buf, size_t count)
 {
+  // read bytes from file descriptor into buffer
   int x = read(fd,buf,count);
   if ( x==-1 )
     { fprintf(stderr, "read() failure at server with message %s\n", strerror(errno)); exitOut(1); }
@@ -62,6 +62,7 @@ int myread(int fd, void *buf, size_t count)
 
 int mywrite(int fd, void *buf, size_t count)
 {
+  // write bytes from buffer into file descriptor
   int x = write(fd,buf,count);
   if ( x==-1 )
     { fprintf(stderr, "write() failure in server with message %s\n",strerror(errno)); exitOut(1); }
@@ -70,6 +71,7 @@ int mywrite(int fd, void *buf, size_t count)
 
 void myclose(int fd)
 {
+  // close the specified file descriptor
   int x = close(fd);
   if (x==-1)
     { fprintf(stderr, "close() failure in server with message %s\n", strerror(errno)); exitOut(1); }
@@ -77,6 +79,7 @@ void myclose(int fd)
 
 void mydup(int fd)
 {
+  // duplicate the specified file descriptor
   int x = dup(fd);
   if (x==-1)
     { fprintf(stderr, "dup() failure at server with message %s\n", strerror(errno)); exitOut(1); }
@@ -89,21 +92,23 @@ void handleSig()
 
 void finishExit(int file)
 {
+  // function to handle normal exists (unexpected exits are handled by exitOut())
   int state;
-  if ( waitpid(ChildID, &state, 0) == -1 )
+  if ( waitpid(ChildID, &state, 0) == -1 ) // wait for the child process terminate
     { fprintf(stderr, "waitpid() failure at server with message %s\n", strerror(errno)); exitOut(1); }
-  if (WIFEXITED(state))
+  if (WIFEXITED(state)) // if the child has terminated notify the user 
     {
       fprintf(stderr, "SHELL EXIT SIGNAL=%d", (state)&0xff);
       fprintf(stderr, " STATUS=%d\n", (state>>8)&0xff); // could also find 0...01111111100000000 number?
     }
-  if ( shutdown(file,SHUT_WR) == -1 )
+  if ( shutdown(file,SHUT_WR) == -1 ) // close server side of tcp connection on port
     { fprintf(stderr, "shutdown() failure at server with message %s\n", strerror(errno)); exitOut(1); }
   exitOut(0);
 }
 
 int establishConnection (int port)
 {
+  // open a TCP socket connection on the specified port. analogous to the function of the same name on client side
   int sockfd;
   if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 )
     { fprintf(stderr, "socket() failure at server with message %s\n", strerror(errno)); exitOut(1); }
@@ -126,6 +131,8 @@ int establishConnection (int port)
 
 void initializeCompression()
 {
+  // initialize uncompression stream (to get compressed data from client) and compression scheme (so that said data can be sent to client).
+  // this is done without loss of generality from how compression/uncompression schemes are set up on the client.
   out_stream.zalloc=Z_NULL;
   in_stream.zalloc=Z_NULL;
   out_stream.zfree=Z_NULL;
@@ -140,7 +147,8 @@ void initializeCompression()
 
 int write_compress(int file, char* buf)
 {
-  int writeSize=1;
+  // write data from buffer to file (client, using compression if specified), analogous to the method of the same name on client.
+  int writeSize=1; // character-at-a-time
   char bufOut[128];
   if (!setCompress)
     writeSize = mywrite(file, buf, 1);
@@ -162,7 +170,8 @@ int write_compress(int file, char* buf)
 
 
 int read_uncompress(int file, char* buf, int readSize)
-{                                                                                                                                                 
+{ 
+  // read data from buffer to file (from client, using decompression if specified), analogous to the method of the same name on client. 
   readSize = myread(file, buf, readSize); // read of 256 is current def.
   if (setCompress)
     {
@@ -183,6 +192,8 @@ int read_uncompress(int file, char* buf, int readSize)
 
 void shellArgCase(char* prog, int file)
 {
+
+  // open unidirectional pipe to child process (shell) 
   int pipeEnteringChild [2];
   int pipeExitingChild [2];
   if ( pipe(pipeEnteringChild) < 0 )
@@ -190,12 +201,14 @@ void shellArgCase(char* prog, int file)
   if ( pipe(pipeExitingChild) < 0 )
     { fprintf(stderr, "Pipe creation error %s\n",strerror(errno)); exitOut(1); }
  
-  ChildID = fork();
-  if (ChildID < 0)
+  ChildID = fork(); // fork a child process (shell)
+  if (ChildID < 0) // fork was unsuccessful
     { fprintf(stderr, "Fork failure with msg %s\n",strerror(errno)); exitOut(1); }
-  else if (ChildID==0) // child process 
+  else if (ChildID==0) // we are in the child process 
     { 
-      childKilled=0;
+      childKilled=0; // child is alive
+	  
+      // rearrange pipes to be able to send and receive outputs from the parent process (terminal) (inter-process-communication)
       myclose( pipeEnteringChild[1] );
       myclose ( pipeExitingChild[0] );
 
@@ -207,16 +220,18 @@ void shellArgCase(char* prog, int file)
       myclose(2);
       mydup( pipeExitingChild[1] );
 
-      if ( execl(prog, prog, (char*)NULL )  == -1 )
+      if ( execl(prog, prog, (char*)NULL )  == -1 ) // runs the specified binary executable
 	{ fprintf(stderr, "Error in executing specified program %s\n", strerror(errno)); exitOut(1); }
     }
   else if (ChildID>0) // then we in parent with child id stored
     {
+      // child is alive, rearrange pipes to be able to send and receive data via IPC to the child shell. 
       childKilled=0;
       myclose( pipeEnteringChild[0] );
       myclose( pipeExitingChild[1] );
       
-      struct pollfd fds[] = { 
+      struct pollfd fds[] = { // poll on input from the user via stdin (which has come from the client since we're 
+	      		      // on the server technically) and from the shell process
 	{ file, POLLIN, 0 },
 	{ pipeExitingChild[0], POLLIN, 0 }
       };
@@ -227,16 +242,16 @@ void shellArgCase(char* prog, int file)
       int res;
       char buf [1024];
       int eof=0;
-      while(!eof)
+      while(!eof) // while no end of file char received
 	{
 	    if ( (res = poll(fds,2,-1)) < 0 )
 	      { fprintf(stderr, "Poll failure with message %s\n", strerror(errno)); exitOut(1); }
-	    else if (res==0)
+	    else if (res==0) // keep waiting, no one is ready
 	      continue;
 
 	    if ( fds[0].revents & POLLIN ) // read from client 
 	    {
-	      int x = read_uncompress(file, buf, 256); // read_uncompress
+	      int x = read_uncompress(file, buf, 256); // read_uncompress performing actual uncompression if that option specified on client end
 	      for (int i=0; i<x; i++)
 	      { 
 		if (buf[i]==0x04)
@@ -248,31 +263,31 @@ void shellArgCase(char* prog, int file)
 		    else
 		      childKilled=1; // mark killed
 		  }
-		else if (buf[i]==cr || buf[i]==lf)
+		else if (buf[i]==cr || buf[i]==lf) // perform cr/lf mapping to <cr><lf>
 		    mywrite(pipeEnteringChild[1], &lf, 1);
-		else
+		else // if not cr/lf write character normally to child process
 		    mywrite(pipeEnteringChild[1], &(buf[i]), 1);
 	      }
 	    }
           
-	    else if ( fds[1].revents & POLLIN ) // write to client
+	    else if ( fds[1].revents & POLLIN ) // read data from the shell since its ready, and write back to client
 	   {	    
-	     int y = myread(pipeExitingChild[0], buf, 256);
+	     int y = myread(pipeExitingChild[0], buf, 256); // perform read of data from shell
 	     for (int i=0; i<y; i++) 
 	     {
-	      if (buf[i]==0x04)
+	      if (buf[i]==0x04) // eof received
 		{ eof=1; break; }
-	      else if (buf[i]==lf)
+	      else if (buf[i]==lf) // lf received, map it to <cr><lf> and write to client (using compression if specified)
 		{ write_compress(file, &cr); write_compress(file, &lf); }
-	      else 
+	      else // normal character received write it normally to client (using compression if specified)
 		write_compress(file, &(buf[i]));
 	     }
 	     fds[1].revents = 0;
 	   }
 
 	    else if ( (fds[0].revents & POLLERR) || (fds[0].revents & POLLHUP) ) //|| (fds[1].revents & POLLERR) )
-	      myclose(pipeEnteringChild[1]);
-	    else if ( (fds[1].revents & POLLERR) || (fds[1].revents & POLLHUP) ) 
+	      myclose(pipeEnteringChild[1]); // if poll error from stdin 
+	    else if ( (fds[1].revents & POLLERR) || (fds[1].revents & POLLHUP) ) // if eof received from shell set eof bit
 	      eof=1;
        }
  
@@ -301,36 +316,36 @@ int main( int argc, char *argv[] )
   char* prog=NULL;
   char* port=NULL;
   static struct option long_options[] = {
-    {"shell", required_argument, 0, 's'},
-    {"port", required_argument, 0, 'p' },
-    {"compress", no_argument, 0, 'c' },
+    {"shell", required_argument, 0, 's'}, // shell option means we will send all data from ourselves to a shell (child)
+    {"port", required_argument, 0, 'p' }, // port number which the server should listen on, and expect client to write data to
+    {"compress", no_argument, 0, 'c' }, // specifies whether client sends/expects-to-receive compressed data from server (us)
     {0,0,0,0}
   };
 
   int in;
   while ( ( in = getopt_long(argc,argv, "", long_options, NULL) ) != -1 )
   {
-      if (in == 's')
+      if (in == 's') // if shell option
 	prog=optarg; 
-      else if (in == 'p')
+      else if (in == 'p') // port number
 	port=optarg;
-      else if (in == 'c')
+      else if (in == 'c') // compression specified
 	setCompress=1;
-      else if (in == '?')
+      else if (in == '?') // unknown arg
       {
 	fprintf(stderr,"Unrecognized argument with message %s\n", strerror(errno));
 	exit(1);
       }
   }
-  if (port==NULL || prog==NULL)
+  if (port==NULL || prog==NULL) // if no port specified or shell option not (unlike in part 1, we wish to send data to shell process everytime)
     { fprintf(stderr, "Must enter arguments --port ' ' and --shell ' ' \n"); exit(1); }
   // set/fill argument struct
   
-  if (setCompress)
+  if (setCompress) // initialize compression scheme if option specified
     initializeCompression();
   
-  int file = establishConnection(atoi(port));
+  int file = establishConnection(atoi(port)); // create a tcp socket connection on specified port we are expecting to receive data on
 
-  shellArgCase(prog,file);
+  shellArgCase(prog,file); // create child process, write data to it from client, receive said data back from shell, and then forward it back to client
  
 }
