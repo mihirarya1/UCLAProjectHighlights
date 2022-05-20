5d3ef09dcb7721c980be9a43da93c3c87cf40f3f
mihirarya1 <73761156+mihirarya1@users.noreply.github.com>
2022-05-19 22:21:54 -0700

    Update fileSystemInterpretation.c

--- FileSystemAnalyzer/fileSystemInterpretation.c
+++ FileSystemAnalyzer/fileSystemInterpretation.c
@@ -23,7 +23,7 @@ struct ext2_inode* inodeSummaryData;
 int groupBlockCount=0, groupInodeCount=0;
 
 int computeOffset(int in)
-{
+{ // compute the byte offset from the start of the file image, of the inputted block number
   return 1024+blockSize*(in-1); // blockNumber of superblock is 1
 }
 
@@ -84,6 +84,7 @@ void computeIndirectionWrapper(int parentInode, int blockNum, int indirectionLev
 
 char* convertTime(time_t inputTime, char returnTime [])
 {
+  // converts inputTime into (mm/dd/yy hh:mm:ss, GMT) time format
   struct tm result;
   gmtime_r(&inputTime, &result);
   sprintf(returnTime, "%02d/%02d/%02d %02d:%02d:%02d", result.tm_mon+1, result.tm_mday, (result.tm_year+1900)%100, result.tm_hour, result.tm_min, result.tm_sec);
@@ -143,56 +144,62 @@ void inodeSummary()
 
 void freeEntries(int groupCount, unsigned int bitmap)
 {
-  int numBytes = (groupCount/8) + !!(groupCount%8);
+  // scans bitmap at hand and checks for all the free blocks 
+	
+  int numBytes = (groupCount/8) + !!(groupCount%8); // size of the bitmap is given by this formula
   char buf [numBytes];
-  if ( pread(fd, &buf, numBytes, computeOffset(bitmap)) == -1 )
+  if ( pread(fd, &buf, numBytes, computeOffset(bitmap)) == -1 ) // read bitmap from file system image into buffer
     { fprintf(stderr,"Error in reading bitmap!\n"); exit(2); }
 
-  for (int l=0; l<numBytes; l++)
-    for (int m=0; m<8; m++)
-	if ( (buf[l]&(1<<m))==0 && (l*8+m+1)<=groupBlockCount )
-	  {
-	    if ( bitmap==groupInfoData.bg_block_bitmap )
+  for (int l=0; l<numBytes; l++) // go byte by byte for bitmap
+    for (int m=0; m<8; m++) // and go bit by bit for each byte
+	if ( (buf[l]&(1<<m))==0 && (l*8+m+1)<=groupBlockCount ) // if m'th bit in l'th byte is not set, and this bit is within the block at hand (groupBlockCount) 
+	  {   							// then it must be free
+	    if ( bitmap==groupInfoData.bg_block_bitmap ) // if we are referring to free block bitmap
 	      printf("BFREE,%d\n", l*8+m+1);
-	    else
+	    else // otherwise we are referring to the inode block bitmap
 	      printf("IFREE,%d\n", l*8+m+1);
 	  }
 }
 
 void groupInfo()
 {
-  if ( pread(fd, &groupInfoData, sizeof(groupInfoData), computeOffset(2)) == -1 )
+  // function to obtain various group metadata for given group in the filesystem
+  if ( pread(fd, &groupInfoData, sizeof(groupInfoData), computeOffset(2)) == -1 ) // read group info into groupInfoData struct
     { fprintf(stderr,"Error reading group summary!\n"); exit(2); }
 
-  groupBlockCount= sb.s_blocks_count;
-  groupInodeCount= sb.s_inodes_count;
+  groupBlockCount= sb.s_blocks_count; // number of blocks in group
+  groupInodeCount= sb.s_inodes_count; // number of inodes in group
   
   printf("GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", 0, groupBlockCount, groupInodeCount, groupInfoData.bg_free_blocks_count, groupInfoData.bg_free_inodes_count, groupInfoData.bg_block_bitmap, groupInfoData.bg_inode_bitmap, groupInfoData.bg_inode_table );
+						// print all group metadata desired
 }
 
 void superblockInfo()
-{
+{ 
+  // function to obtain superblock information from the file system image
   if ( pread(fd, &sb, sizeof(sb), computeOffset(1)) == -1 )
     { fprintf(stderr,"Error reading superblock!\n"); exit(2); }
-  if ( sb.s_magic != EXT2_SUPER_MAGIC )
+  if ( sb.s_magic != EXT2_SUPER_MAGIC ) // expected value to be stored in suberblock.s_magic is EXT2_SUPER_MAGIC, else didn't read superblock correctly
     { fprintf(stderr,"Did not correctly read superblock!\n"); exit(2); }
 
   blockSize = EXT2_MIN_BLOCK_SIZE << sb.s_log_block_size;
   printf( "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n", sb.s_blocks_count, sb.s_inodes_count, blockSize, sb.s_inode_size, sb.s_blocks_per_group, sb.s_inodes_per_group, sb.s_first_ino  );
+	                                      // above print statement contains all the superblock metadata desired
 }
 
 int main(int argc,  char *argv[] )
 {
-  if ( argc!=2 || argv[1]==NULL )
+  if ( argc!=2 || argv[1]==NULL ) // we want exactly one input argument, and that should be the name of the file containing the file system image
     { fprintf(stderr,"Invalid input arguments!\n"); exit(1); }
-  fd = open(argv[1], O_RDONLY);
+  fd = open(argv[1], O_RDONLY); // open file descriptor for file system image
   if ( fd == -1 )
     { fprintf(stderr,"Unable to open specified file\n"); exit(1); }
   
   superblockInfo();
-  groupInfo();
-  freeEntries(groupBlockCount, groupInfoData.bg_block_bitmap);
-  freeEntries(groupInodeCount, groupInfoData.bg_inode_bitmap);
+  groupInfo(); // call for each group
+  freeEntries(groupBlockCount, groupInfoData.bg_block_bitmap); // scan free block bitmap for group at hand
+  freeEntries(groupInodeCount, groupInfoData.bg_inode_bitmap); // scan free inode bitmap for group at hand
   inodeSummary();
   
   free(inodeSummaryData);
